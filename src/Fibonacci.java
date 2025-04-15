52027642325f61f9e8f81f67ae05a97245abd35a
suvethaa <suvethaj07@gmail.com>
2025-04-15 14:08:59 +0530

    Running first program ofJavaBootcamp

---
+++ src/Fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Fibonacci{
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+
+        int a = 0 ;
+        int b = 1 ;
+        int count = 2 ;
+
+        while(count<2){
+            int temp = a ;
+
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter some input : ");
+        int num = scan.nextInt();
+        System.out.println("The number is : "+num);
+    }
+}
