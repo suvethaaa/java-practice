a0055390b83147ed5a87aad75ec3df127f02b8b2
suvethaa <suvethaj07@gmail.com>
2025-04-16 19:52:07 +0530

    Problem Solving

---
+++ src/VarArgs.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+           show(8,9,"kunal","kushwaha");
+          // fun(56,67,89);
+    }
+
+
+    static void fun(int ...m) {//when you want to pass a number of args bt not sure about the number
+        System.out.println(Arrays.toString(m));
+    }
+
+    //passing multiple variable note:the varargs must be defined in last and when passing args that should follow the order
+    //ex.int a, int b , int (dont forget the space ) ...(three dots) 'n' (can be anything)
+
+    static void  show(int a , int b , String ...s){
+        System.out.println(a + "  " + b + " " + Arrays.toString(s));
+    }
+
+
+}
