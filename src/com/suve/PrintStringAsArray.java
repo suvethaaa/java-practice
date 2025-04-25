732f4805d06d86408f72d8f8e1ca42bf7b3efe6e
suvethaa <suvethaj07@gmail.com>
2025-04-25 11:52:57 +0530

    Problem Solving

---
+++ src/com/suve/PrintStringAsArray.java
@@ -0,0 +1,8 @@
+package com.suve;
+import java.util.Arrays;
+public class PrintStringAsArray {
+    public static void main(String[] args) {
+        String str = "suvetha";
+        System.out.println(Arrays.toString(str.toCharArray()));
+    }
+}
