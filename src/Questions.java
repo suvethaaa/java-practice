ed18d554cf839f3d41e4000d958a9a1175877ffe
suvethaa <suvethaj07@gmail.com>
2025-04-25 09:39:28 +0530

    Problem Solving

--- src/Questions.java
+++ src/Questions.java
@@ -4,9 +4,16 @@ public class Questions {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a number to check prime ");
-        int n = scan.nextInt();
-        System.out.println(isPrime(n));
+//        System.out.println("Enter a number to check prime ");
+//        int n = scan.nextInt();
+//        System.out.println(isArmstrong(n));
+//        System.out.println(isPrime(n));
+
+        for(int i = 100 ; i < 1000 ; i ++){
+            if(isArmstrong(i)){
+                System.out.println(i);
+            }
+        }
     }
 
     static boolean isPrime(int n) {
@@ -22,5 +29,17 @@ public class Questions {
 
         }
         return c * c > n;
+    }
+
+    static boolean isArmstrong(int n){
+        int original = n ;
+        int sum = 0 ;
+
+        while(n > 0){
+            int rem = n % 10 ;
+            sum = sum + rem*rem*rem;
+            n = n / 10;
+        }
+        return original==sum;
     }
 }
