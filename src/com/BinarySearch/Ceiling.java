2f60c8fa1be6d4ca9655d1e29005d029048507c3
suvethaa <suvethaj07@gmail.com>
2025-04-27 08:45:41 +0530

    Problem Solving

---
+++ src/com/BinarySearch/Ceiling.java
@@ -0,0 +1,30 @@
+package com.BinarySearch;
+
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] nums = {2,3,5,9,14,16,18};
+        int target = 4;
+        System.out.println(ceilingBS(nums,target));
+    }
+
+    static int ceilingBS(int[] arr , int target){
+        int start = 0 ;
+        int end = arr.length-1;
+
+        if(target>arr[end]){
+            return end ;
+        }
+        while(start<=end){
+            int mid = start+(end-start)/2;
+
+            if(target<arr[mid]){
+                end = mid - 1 ;
+            }else if(target>arr[mid]){
+                start = mid+1;
+            }else if(target==arr[mid]){
+                return mid;
+            }
+        }
+        return start;
+    }
+}
