2ce19b1219228ee48041c52936a088c70b91268b
suvethaa <suvethaj07@gmail.com>
2025-04-29 21:11:39 +0530

    Problem Solving

---
+++ src/com/BinarySearch/FindInRotatedSortedArray.java
@@ -0,0 +1,39 @@
+package com.BinarySearch;
+
+public class FindInRotatedSortedArray {
+    public static void main(String[] args) {
+
+    }
+
+    static int pivot(int[] arr, int target){
+        int start = 0 ;
+        int end = arr.length-1;
+
+        while(start<=end){
+            int mid = start+(end-start)/2;
+
+            if(mid < end && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            if(mid > start && arr[mid]<arr[mid-1]){
+                return mid-1;
+            }
+            if(arr[mid]==arr[start] && arr[mid]==arr[end]){
+                if(arr[start]>arr[start+1]){
+                    return start;
+                }
+                start++;
+                if(arr[end]<arr[end-1]){
+                    return end-1;
+                }
+                end--;
+            }
+            else if(arr[start]<arr[mid] || arr[start] == arr[mid] && arr[mid]>arr[end]){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return -1 ;
+    }
+}
