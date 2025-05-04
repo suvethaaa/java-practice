57c5889b3bf7e0639d04ecf442b128d93c1633b9
suvethaa <suvethaj07@gmail.com>
2025-05-04 19:32:32 +0530

    Problem Solving

---
+++ src/com/sorting/MissingNumber.java
@@ -0,0 +1,35 @@
+package com.sorting;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2};
+        System.out.println( missingNumber(arr));
+    }
+
+    static int missingNumber(int[] arr){
+        int i = 0;
+
+        while(i< arr.length){
+
+            int correctIndex = arr[i];
+            if(arr[i] < arr.length && arr[i] != arr[correctIndex]){
+                swap(arr,i,correctIndex);
+            }else{
+                i++;
+            }
+        }
+
+        for (int index = 0; index < arr.length; index++) {
+            if(arr[index] != index){
+                return index;
+            }
+        }
+        return arr.length;
+    }
+
+    static void swap(int[] arr , int first , int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
