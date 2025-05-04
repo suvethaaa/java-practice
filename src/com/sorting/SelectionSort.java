6a52d39c404869b0da761185fc54adefd127a77f
suvethaa <suvethaj07@gmail.com>
2025-05-04 19:35:30 +0530

    Problem Solving

---
+++ src/com/sorting/SelectionSort.java
@@ -0,0 +1,37 @@
+package com.sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+
+        int[] arr = {4,5,1,2,3,-89,-72,399,0,9,8};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void selection(int[] arr){
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int lastIndex = arr.length - i - 1;
+            int Max = max(arr,0,lastIndex);
+            swap(arr,Max,lastIndex);
+        }
+    }
+
+    static int max(int[]arr , int start , int end){
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if(arr[max]<arr[i]){
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    static void swap(int[] arr, int max , int lastIndex){
+        int temp = arr[max];
+        arr[max] = arr[lastIndex];
+        arr[lastIndex] = temp;
+    }
+}
