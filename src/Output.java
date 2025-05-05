09a935ee7d3121e1ef6821a872310aba7b2732db
suvethaa <suvethaj07@gmail.com>
2025-05-05 20:43:43 +0530

    Problem Solving

--- src/Output.java
+++ src/Output.java
@@ -1,5 +1,26 @@
-public class Output{
+import java.util.Arrays;
+
+public class Output {
     public static void main(String[] args) {
-        System.out.println("suvetha");
+        int[] arr = {2,7,11,15};
+        System.out.println(Arrays.toString(twoSum(arr,9)));
+    }
+    static int[] twoSum(int[] numbers, int target) {
+        int start = 1;
+        int end = numbers.length;
+
+        while(start<=end){
+            int otherElement = target - numbers[start];
+            int mid = start+(end-start)/2;
+
+            if(otherElement<mid){
+                end = mid-1;
+            }else if(otherElement>mid){
+                start = mid+1;
+            }else{
+                return new int[]{start,mid};
+            }
+        }
+        return new int[]{-1,-1};
     }
 }
