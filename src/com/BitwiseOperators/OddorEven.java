6afc049ab4783f8155b706d703f1834231ce34ad
suvethaa <suvethaj07@gmail.com>
2025-05-12 19:31:35 +0530

    recursion practice

--- src/com/BitwiseOperators/OddorEven.java
+++ src/com/BitwiseOperators/OddorEven.java
@@ -2,6 +2,13 @@ package com.BitwiseOperators;
 
 public class OddorEven {
     public static void main(String[] args) {
-
+        int n = 9;
+        System.out.println(isOdd(n));
+    }
+    // the only bit determines whether  it is an odd or even is the least significant bit and if it is one then
+    //the number is odd if its 1 the number is even if its zero
+    //& --> bitwise operator
+    static boolean isOdd(int n) {
+        return (n & 1) == 1;
     }
 }
