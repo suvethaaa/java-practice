ef9048eb32b9f3fc435186dd42aa926a0359ac21
suvethaa <suvethaj07@gmail.com>
2025-08-03 20:18:59 +0530

    Maze Problems using Backtracking

---
+++ src/com/Backtracking/MazePath.java
@@ -0,0 +1,21 @@
+package com.Backtracking;
+
+public class MazePath {
+    public static void main(String[] args) {
+        mazePath("",3,3);
+    }
+
+    static void mazePath(String p , int r , int c){
+        if(r == 1 && c == 1){
+            System.out.println(p);
+            return;
+        }
+
+        if(r > 1){
+            mazePath(p + 'D' , r-1 , c);
+        }
+        if(c > 1){
+            mazePath(p + 'R' , r , c-1);
+        }
+    }
+}
