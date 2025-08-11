f1d0a815c45bfe7ac696003fd54c3d7221a17812
suvethaa <suvethaj07@gmail.com>
2025-08-11 11:29:08 +0530

    Maze Problems using Backtracking

---
+++ src/com/Backtracking/AllPathRestrictions.java
@@ -0,0 +1,42 @@
+package com.Backtracking;
+
+public class AllPathRestrictions {
+    public static void main(String[] args) {
+
+        boolean[][] board = {
+                {true,true,true},
+                {true,true,true},
+                {true,true,true}
+        };
+
+        allPathRestrictions("" , board , 0 , 0);
+    }
+
+    static void allPathRestrictions(String p , boolean[][] maze, int r , int c){
+
+        if(r == maze.length -1 && c == maze[0].length -1){
+            System.out.println(p);
+            return;
+        }
+
+        if(!maze[r][c]){
+            return;
+        }
+
+        if(r < maze.length-1){
+            allPathRestrictions(p + 'D' , maze , r +1 , c);
+        }
+
+        if(c < maze.length-1){
+            allPathRestrictions(p + 'R' , maze , r , c+1);
+        }
+
+        if(r > 0){
+            allPathRestrictions(p + 'U' , maze , r -1 , c);
+        }
+
+        if(c > 0){
+            allPathRestrictions(p + 'D' , maze , r , c -1 );
+        }
+    }
+}
