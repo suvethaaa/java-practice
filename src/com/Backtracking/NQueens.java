f1b534c51febd955ec432263e9448a69c4c7de1d
suvethaa <suvethaj07@gmail.com>
2025-08-22 20:10:46 +0530

    Maze Problems using Backtracking

---
+++ src/com/Backtracking/NQueens.java
@@ -0,0 +1,73 @@
+package com.Backtracking;
+
+public class NQueens {
+    public static void main(String[] args) {
+        int n = 4 ;
+        boolean[][] board = new boolean[n][n];
+        System.out.println(queens(board,0));
+    }
+
+    static int queens(boolean[][] board , int row){
+
+        //base condition
+        if(row == board.length){
+            display(board);
+            System.out.println();
+            return 1;
+        }
+        int count = 0;
+        //placing the queens
+
+        for (int col = 0; col < board.length; col++) {
+            if(isSafe(board , col , row)){
+                board[row][col] = true;
+                count+= queens(board , row +1);
+                board[row][col] = false;
+            }
+        }
+        return count;
+    }
+
+    static boolean isSafe(boolean[][] board, int col, int row) {
+
+        //check vertical row
+        for (int i = 0; i < row; i++) {
+            if(board[i][col]){
+                return false;
+            }
+        }
+
+        //diagonal left
+
+        int maxLeft = Math.min(row,col);
+
+        for (int i = 1; i <= maxLeft; i++) {
+            if(board[row - i][col - i]){
+                return false;
+            }
+        }
+        //check right diagonal
+        int maxRight = Math.min(row , board.length - col -1);
+        for (int i = 1; i <= maxRight; i++) {
+            if(board[row - i][col + i]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void display(boolean[][] board){
+
+        for(boolean[] row : board){
+            for(boolean element : row){
+                if(element){
+                    System.out.print("Q ");
+                }else{
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
