c84eefdacc8f60917e57439e11a550fae521abe8
suvethaa <suvethaj07@gmail.com>
2025-08-23 13:32:41 +0530

    N-Knights using Backtracking

---
+++ src/com/Backtracking/NKnights.java
@@ -0,0 +1,85 @@
+package com.Backtracking;
+
+public class NKnights {
+    public static void main(String[] args) {
+        int n = 4;
+        boolean[][] board = new boolean[n][n];
+        knights(board,0,0,n);
+    }
+
+    static void knights(boolean[][] board , int row , int col , int knights){
+
+        if(knights == 0){
+            display(board);
+            System.out.println();
+            return ;
+        }
+
+        if(row == board.length -1 && col == board[0].length ){
+            return ;
+        }
+
+        if(col == board[0].length){
+            knights(board,row+1,0,knights);
+            return ;
+        }
+
+        if(isSafe(board,row,col)){
+            board[row][col] = true;
+            knights(board,row,col+1,knights -1);
+            board[row][col] = false;
+        }
+
+        knights(board,row,col+1,knights);
+    }
+
+    static boolean isSafe(boolean[][] board , int row , int col){
+        //there are 8 coordinates that ca attack the knight, but we are checking only the above coordinates
+        //because no below knights are placed, yet we are only trying to place the knights from the left and below
+        if(isValid(board ,row-1 , col-2)){
+            if(board[row-1][col-2]){
+                return false;
+            }
+        }
+
+        if(isValid(board ,row-1 , col+2)){
+            if(board[row-1][col+2]){
+                return false;
+            }
+        }
+
+        if(isValid(board ,row-2 , col-1)){
+            if(board[row-2][col-1]){
+                return false;
+            }
+        }
+
+        if(isValid(board ,row-2 , col+1)){
+            if(board[row-2][col+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean isValid(boolean[][] board , int row , int col){
+        if(row >= 0 && row < board.length && col >= 0 && col < board.length){
+            return true;
+        }
+        return false;
+    }
+
+    static void display(boolean[][] board){
+
+        for(boolean[] arr : board){
+            for(boolean element : arr){
+                if(element){
+                    System.out.print("K ");
+                }else{
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
