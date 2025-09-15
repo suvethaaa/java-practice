c93aca932b0182f0f72f96eb12364a4905f81882
suvethaa <suvethaj07@gmail.com>
2025-09-15 19:23:48 +0530

    dsa

--- src/com/BinarySearch/Ceiling.java
+++ src/com/BinarySearch/Ceiling.java
@@ -3,7 +3,7 @@ package com.BinarySearch;
 public class Ceiling {
     public static void main(String[] args) {
         int[] nums = {2,3,5,9,14,16,18};
-        int target = 19;
+        int target = 1;
         System.out.println(ceilingBS(nums,target));
     }
 
@@ -14,14 +14,17 @@ public class Ceiling {
         if(target>arr[end]){
             return end ;
         }
-        while(start<=end){
-            int mid = start+(end-start)/2;
+        if(target<arr[start]){
+            return -1;
+        }
+        while(start<=end) {
+            int mid = start + (end - start) / 2;
 
-            if(target<arr[mid]){
-                end = mid - 1 ;
-            }else if(target>arr[mid]){
-                start = mid+1;
-            }else if(target==arr[mid]){
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target == arr[mid]) {
                 return mid;
             }
         }
