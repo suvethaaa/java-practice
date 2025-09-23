f4b2a3186fa7688ac93990e38ca1143043afc05d
suvethaa <suvethaj07@gmail.com>
2025-09-23 18:47:19 +0530

    problem solving

--- src/com/Problems/TotalSundays.java
+++ src/com/Problems/TotalSundays.java
@@ -3,27 +3,27 @@ package com.Problems;
 public class TotalSundays {
     public static void main(String[] args) {
         String day = "sun";
-        int n= 8;
-        System.out.println(findSundays(n , day));
+        int n = 8;
+        System.out.println(findSundays(n, day));
     }
 
     public static int getDayIndex(String startDay) {
-        switch(startDay.toLowerCase()) {
-            case "mon": return 0;
-            case "tue": return 1;
-            case "wed": return 2;
-            case "thu": return 3;
-            case "fri": return 4;
-            case "sat": return 5;
-            case "sun": return 6;
-            default: throw new IllegalArgumentException("Invalid day input");
-        }
+        return switch (startDay.toLowerCase()) {
+            case "mon" -> 0;
+            case "tue" -> 1;
+            case "wed" -> 2;
+            case "thu" -> 3;
+            case "fri" -> 4;
+            case "sat" -> 5;
+            case "sun" -> 6;
+            default -> throw new IllegalArgumentException("Invalid day input");
+        };
     }
 
-    static int findSundays(int n , String day) {
+    static int findSundays(int n, String day) {
 
-        int fullWeeks = n/7;
-        int remainingDays = n%7;
+        int fullWeeks = n / 7;
+        int remainingDays = n % 7;
         int dayIndex = getDayIndex(day);
         int sundays = fullWeeks;
 
